92615bc16b0aeb44ca2214aa54d9f2b2ff22c521
onurae <oakin.ae@gmail.com>
2022-11-01 20:48:07 +0300

    Add baro files

---
+++ main/baro.cpp
@@ -0,0 +1,208 @@
+/*******************************************************************************************
+ *                                                                                         *
+ *    ESP32-FC                                                                             *
+ *    Barometric Pressure Sensor (MS5611)                                                  *
+ *                                                                                         *
+ *    Copyright (c) 2022 Onur AKIN <https://github.com/onurae>                             *
+ *    Licensed under the MIT License.                                                      *
+ *                                                                                         *
+ ******************************************************************************************/
+
+#include "baro.hpp"
+
+bool Baro::Init()
+{
+    if (i2c->Write(addressMS5611, 0x1E) != ESP_OK) // Reset
+    {
+        printf("MS5611 not found!\n");
+        return false;
+    }
+    printf("MS5611 found.\n");
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    for (int i = 0; i < 6; i++) // Read calibration data
+    {
+        uint8_t buf[2];
+        ESP_ERROR_CHECK(i2c->Read(addressMS5611, 0xA2 + (i * 2), 2, buf));
+        cal[i] = ((uint16_t)buf[0]) << 8 | (uint16_t)buf[1];
+    }
+    printf(" C1: %d\n C2: %d\n C3: %d\n C4: %d\n C5: %d\n C6: %d\n", cal[0], cal[1], cal[2], cal[3], cal[4], cal[5]);
+    tRef = (int64_t)cal[4] << 8;
+    tOff1 = (int64_t)cal[1] << 16;
+    tSens1 = (int64_t)cal[0] << 15;
+
+    if (SendConvCmdTemp() == false) // Send temperature command
+    {
+        printf("MS5611 temperature command error.\n");
+        return false;
+    }
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+    if (ReadTemperature() == false) // Read digital temperature value
+    {
+        printf("MS5611 temperature read error.\n");
+        return false;
+    }
+    if (SendConvCmdPres() == false) // Send pressure command
+    {
+        printf("MS5611 pressure command error.\n");
+        return false;
+    }
+    lastConvTick = xTaskGetTickCount();
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+    CalculatePresTempAlt(); // Read digital pressure value, calculate pressure, temperature and altitude.
+    altfp = alt;
+    PrintPresTemp();
+    PrintAltVs();
+    printf("MS5611 ready.\n");
+    return true;
+}
+
+void Baro::Update(float dt)
+{
+    CalculatePresTempAlt();
+    CalculateFilteredAltVs(dt);
+}
+
+bool Baro::SendConvCmdPres()
+{
+    if (i2c->Write(addressMS5611, addDpt[0]) != ESP_OK)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool Baro::SendConvCmdTemp()
+{
+    if (i2c->Write(addressMS5611, addDpt[1]) != ESP_OK)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool Baro::ReadPressure()
+{
+    uint8_t buf[3];
+    if (i2c->Read(addressMS5611, 0x00, 3, buf) != ESP_OK)
+    {
+        return false;
+    }
+    dpt[0] = ((uint32_t)buf[0] << 16) | ((uint32_t)buf[1] << 8) | (uint32_t)buf[2];
+    if (dpt[0] == 0) // if loop is fast, it returns 0.
+    {
+        return false;
+    }
+    return true;
+}
+
+bool Baro::ReadTemperature()
+{
+    uint8_t buf[3];
+    if (i2c->Read(addressMS5611, 0x00, 3, buf) != ESP_OK)
+    {
+        return false;
+    }
+    dpt[1] = ((uint32_t)buf[0] << 16) | ((uint32_t)buf[1] << 8) | (uint32_t)buf[2];
+    if (dpt[1] == 0) // if loop is fast, it returns 0.
+    {
+        return false;
+    }
+    return true;
+}
+
+bool Baro::ReadPresTemp()
+{
+    uint32_t currentTick = xTaskGetTickCount();
+    if ((currentTick - lastConvTick) < pdMS_TO_TICKS(10))
+    {
+        return false;
+    }
+    lastConvTick = currentTick;
+
+    // If the sample rate is 100Hz, pressure & temperature refresh rate would be 50Hz.
+    if (state == false)
+    {
+        bool pState = ReadPressure();
+        bool tState = SendConvCmdTemp();
+        if (pState && tState)
+        {
+            state = true;
+            return true;
+        }
+    }
+    else
+    {
+        bool tState = ReadTemperature();
+        bool pState = SendConvCmdPres();
+        if (tState && pState)
+        {
+            state = false;
+            return true;
+        }
+    }
+    return false;
+}
+
+void Baro::CalculatePresTempAlt()
+{
+    if (ReadPresTemp() == true)
+    {
+        tDiff = (int64_t)dpt[1] - tRef;
+        tAct = 2000 + ((tDiff * cal[5]) >> 23);
+        tOff = tOff1 + ((tDiff * cal[3]) >> 7);
+        tSens = tSens1 + ((tDiff * cal[2]) >> 8);
+
+        if (tAct < 2000) // Temperature < 20°C
+        {
+            tAct2 = (tDiff * tDiff) >> 31;
+            int64_t x = tAct - 2000;
+            x = 5 * x * x;
+            tOff2 = x >> 1;
+            tSens2 = x >> 2;
+            if (tAct < -1500) // Temperature < -15°C
+            {
+                x = tAct + 1500;
+                x = x * x;
+                tOff2 += 7 * x;
+                tSens2 += ((11 * x) >> 2);
+            }
+            tAct -= tAct2;
+            tOff -= tOff2;
+            tSens -= tSens2;
+        }
+
+        temperature = tAct / 100.0f;
+        tCompPres = (((((int64_t)dpt[0] * tSens) >> 21) - tOff) >> 15);
+        pressure = tCompPres / 100.0f;
+        alt = (((tsl + 273.15f) / -0.0065f) * (std::pow(pressure / qnh, 0.1902632f) - 1.0f));
+    }
+}
+
+float Baro::CalculateAlpha(float f, float dt)
+{
+    if (f < 0.0f)
+    {
+        return 1.0f;
+    }
+    float omega = f * 2.0f * M_PI;
+    return (omega * dt / (1.0f + omega * dt));
+}
+
+void Baro::CalculateFilteredAltVs(float dt)
+{
+    float altAlpha = CalculateAlpha(fCutAlt, dt);
+    altf = (1.0f - altAlpha) * altfp + altAlpha * alt; // Filtered altitude
+    vs = (altf - altfp) / dt;                          // Vertical speed
+    altfp = altf;
+}
+
+void Baro::PrintPresTemp()
+{
+    printf("%s%.2f, %s%.2f\n", "pres: ", pressure, "temp: ", temperature);
+}
+
+void Baro::PrintAltVs()
+{
+    printf("alt: %.2f, altf: %.2f, vs: %.2f\n", alt, altf, vs);
+}
---
+++ main/baro.hpp
@@ -0,0 +1,64 @@
+/*******************************************************************************************
+ *                                                                                         *
+ *    ESP32-FC                                                                             *
+ *    Barometric Pressure Sensor (MS5611)                                                  *
+ *                                                                                         *
+ *    Copyright (c) 2022 Onur AKIN <https://github.com/onurae>                             *
+ *    Licensed under the MIT License.                                                      *
+ *                                                                                         *
+ ******************************************************************************************/
+
+#ifndef BARO_HPP
+#define BARO_HPP
+
+#include "i2c.hpp"
+#include <cmath>
+
+class Baro
+{
+private:
+    I2c *i2c;                           // I2C
+    const uint8_t addressMS5611 = 0x77; // CSB: Low
+    uint16_t cal[7];                    // Calibration data
+    uint32_t dpt[2];                    // Digital pressure & temperature value
+    uint8_t addDpt[2] = {0x48, 0x58};   // OCR: 4096. Conversion time: 9.04 [ms] max.
+    int64_t tDiff;                      // Difference between actual and reference temperature
+    int32_t tAct;                       // Actual temperature
+    int64_t tOff;                       // Offset at actual temperature
+    int64_t tSens;                      // Sensitivity at actual temperature
+    int32_t tCompPres;                  // Temperature compensated pressure
+    int64_t tRef;                       // Reference temperature
+    int64_t tOff1;
+    int64_t tSens1;
+    int32_t tAct2;
+    int64_t tOff2;
+    int64_t tSens2;
+    bool SendConvCmdPres();
+    bool SendConvCmdTemp();
+    bool ReadPressure();
+    bool ReadTemperature();
+    uint32_t lastConvTick;
+    bool ReadPresTemp(); // max 50Hz.
+    void CalculatePresTempAlt();
+    float temperature = 0; // [°C]
+    float pressure = 0;    // [mbar]
+    float qnh = 1019.00;   // Mean sea level pressure [mbar]
+    float tsl = 15.0;      // Temperature sea level [°C]
+    float alt = 0;         // Altitude sea level [m]
+    bool state = false;    // false: Pres read, temp cmd. true: Pres cmd, temp read.
+    void CalculateFilteredAltVs(float dt);
+    float CalculateAlpha(float f, float dt);
+    float altf = 0;      // Filtered altitude [m]
+    float vs = 0;        // Vertical speed [m/s]
+    float altfp = 0;     // Previous filtered altitude
+    float fCutAlt = 0.1; // [Hz]
+public:
+    Baro(I2c *i2c) { this->i2c = i2c; }
+    virtual ~Baro() = default;
+    bool Init();
+    void Update(float dt);
+    void PrintPresTemp();
+    void PrintAltVs();
+};
+
+#endif /* BARO_HPP */
