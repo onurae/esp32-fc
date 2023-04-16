589403c69952a5808a00cfb0c9575addb6046c1a
onurae <oakin.ae@gmail.com>
2023-04-16 23:16:26 +0300

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -19,7 +19,7 @@
 #include "battery.hpp"
 #include "frsky.hpp"
 
-extern "C" void Task1(void *params)
+extern "C" void Task2(void *params)
 {
     printf("Telemetry task has been started!\n");
 
@@ -28,9 +28,9 @@ extern "C" void Task1(void *params)
     printf("%d\n", size);
 
     // int *p = (int *)params;
-    //gpio_num_t led = GPIO_NUM_22;
-    //gpio_set_direction(led, GPIO_MODE_OUTPUT);
-    //int isOn = 0;
+    // gpio_num_t led = GPIO_NUM_22;
+    // gpio_set_direction(led, GPIO_MODE_OUTPUT);
+    // int isOn = 0;
 
     Battery battery;
     battery.Init();
@@ -40,33 +40,38 @@ extern "C" void Task1(void *params)
     frsky.Init();
     frsky.Flush();
 
+    int64_t currentTime = esp_timer_get_time();
+    int i = 0;
+
     TickType_t xLastWakeTime = xTaskGetTickCount();
     while (true)
     {
-        //printf("Task1 is running!\n");
-        //isOn = !isOn;
-        //gpio_set_level(led, isOn);
-        frsky.SetVoltage(battery.GetVoltage() / 1000.0f);
+        int64_t workTime = esp_timer_get_time(); // [us]
+        // printf("w: %d\n", (uint16_t)(workTime - currentTime));
+        vTaskDelay(pdMS_TO_TICKS(2));
+        currentTime = esp_timer_get_time(); // [us]
+
+        // printf("Task1 is running!\n");
+        // isOn = !isOn;
+        // gpio_set_level(led, isOn);
+        i += 1;
+        if (i > 500) // approximately 500*2 = 1 seconds.
+        {
+            frsky.SetVoltage(battery.GetVoltage() / 1000.0f);
+            i = 0;
+        }
         frsky.Operate();
-        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(2)); // Critical frequency.
     }
     // vTaskDelete( NULL );
 }
 
-extern "C" void app_main(void)
+extern "C" void Task1(void *params)
 {
-    PrintCountDown("Starting in", 10);
+    printf("Main task has been started!\n");
 
-    // Frsky frsky;
-    // frsky.Init();
-    // while(true)
-    //{
-    //     //frsky.Run1();
-    //     frsky.Operate();
-    //     vTaskDelay(2 / portTICK_PERIOD_MS);
-    // }
-    xTaskCreate(Task1, "myFirstTask", 4096, NULL, 0, NULL); // Low priority, periodic task.
-    BlinkLedForever();
+    size_t size = xPortGetFreeHeapSize();
+    printf("FreeHeapSize: ");
+    printf("%d\n", size);
 
     I2c i2c(0, 19, 23); // Port, SCL, SDA
     i2c.MasterInit();   // Initialize I2C
@@ -120,10 +125,11 @@ extern "C" void app_main(void)
     int64_t prevTime = esp_timer_get_time();
     int64_t currentTime = prevTime;
     float dt = 0;
-    TickType_t xLastWakeTime = xTaskGetTickCount();
 
+    xTaskCreate(Task2, "mySecondTask", 4096, NULL, 0, NULL); // Low priority.
     sbus.Flush();
-    for (;;)
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+    while (true)
     {
         // int64_t workTime = esp_timer_get_time();         // [us]
         // printf("w: %d\n", (uint16_t)(workTime - currentTime));
@@ -151,7 +157,7 @@ extern "C" void app_main(void)
         //  ahrs.PrintQuaternions();
         // ahrs.PrintEulerAngles();
 
-        baro.Update(dt);
+        // baro.Update(dt);
         // baro.PrintAltVs();
 
         if (sbus.Read())
@@ -162,10 +168,6 @@ extern "C" void app_main(void)
         }
         // printf("%d\n", sbus.CheckStatus());
 
-        // make this task!
-        // frsky.SetVoltage(battery.GetVoltage() / 1000.0f);
-        // frsky.Operate();
-
         bool throttleCut = false;
         if (throttleCut == true)
         {
@@ -175,4 +177,11 @@ extern "C" void app_main(void)
             esc4.Update(1000);
         }
     }
+}
+
+extern "C" void app_main(void)
+{
+    PrintCountDown("Starting in", 10);
+    xTaskCreate(Task1, "myFirstTask", 4096, NULL, 1, NULL); // High priority, periodic task.
+    // while ?
 }
