56c1c5d076d9c8566b86ad076de566898017983c
onurae <oakin.ae@gmail.com>
2023-04-16 18:42:34 +0300

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -17,15 +17,57 @@
 #include "servo.hpp"
 #include "esc.hpp"
 #include "battery.hpp"
+#include "frsky.hpp"
+
+extern "C" void Task1(void *params)
+{
+    printf("Telemetry task has been started!\n");
+
+    size_t size = xPortGetFreeHeapSize();
+    printf("FreeHeapSize: ");
+    printf("%d\n", size);
+
+    // int *p = (int *)params;
+    //gpio_num_t led = GPIO_NUM_22;
+    //gpio_set_direction(led, GPIO_MODE_OUTPUT);
+    //int isOn = 0;
+
+    Battery battery;
+    battery.Init();
+    printf("Battery Voltage: %d\n", battery.GetVoltage());
+
+    Frsky frsky;
+    frsky.Init();
+    frsky.Flush();
+
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+    while (true)
+    {
+        //printf("Task1 is running!\n");
+        //isOn = !isOn;
+        //gpio_set_level(led, isOn);
+        frsky.SetVoltage(battery.GetVoltage() / 1000.0f);
+        frsky.Operate();
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(2)); // Critical frequency.
+    }
+    // vTaskDelete( NULL );
+}
 
 extern "C" void app_main(void)
 {
     PrintCountDown("Starting in", 10);
-    Battery battery;
-    battery.Init();
-    printf("Battery Voltage: %d\n", battery.GetVoltage());
+
+    // Frsky frsky;
+    // frsky.Init();
+    // while(true)
+    //{
+    //     //frsky.Run1();
+    //     frsky.Operate();
+    //     vTaskDelay(2 / portTICK_PERIOD_MS);
+    // }
+    xTaskCreate(Task1, "myFirstTask", 4096, NULL, 0, NULL); // Low priority, periodic task.
     BlinkLedForever();
-    
+
     I2c i2c(0, 19, 23); // Port, SCL, SDA
     i2c.MasterInit();   // Initialize I2C
 
@@ -51,20 +93,16 @@ extern "C" void app_main(void)
 
     Sbus sbus;
     sbus.Init();
-    while(!sbus.IsReady())
-    {
-        Wait("Sbus data...", 3);
-    }
 
-    //ServoTimer servoTimer(0);
-    //ServoOperator servoOperator1(&servoTimer);
-    //Servo servo1(&servoOperator1, GPIO_NUM_27, 1000, 2000, -45, +45, 1200);
-    //Servo servo2(&servoOperator1, GPIO_NUM_26, 1000, 2000, -45, +45, 1500);
-    //ServoOperator servoOperator2(&servoTimer);
-    //Servo servo3(&servoOperator2, GPIO_NUM_25, 1000, 2000, -45, +45, 1700);
-    //Servo servo4(&servoOperator2, GPIO_NUM_33, 1000, 2000, -45, +45, 1400);
-    //servoTimer.EnableAndStartTimer();
-    //ServoTest(&servo1, &servo2, &servo3, &servo4);
+    // ServoTimer servoTimer(0);
+    // ServoOperator servoOperator1(&servoTimer);
+    // Servo servo1(&servoOperator1, GPIO_NUM_27, 1000, 2000, -45, +45, 1200);
+    // Servo servo2(&servoOperator1, GPIO_NUM_26, 1000, 2000, -45, +45, 1500);
+    // ServoOperator servoOperator2(&servoTimer);
+    // Servo servo3(&servoOperator2, GPIO_NUM_25, 1000, 2000, -45, +45, 1700);
+    // Servo servo4(&servoOperator2, GPIO_NUM_33, 1000, 2000, -45, +45, 1400);
+    // servoTimer.EnableAndStartTimer();
+    // ServoTest(&servo1, &servo2, &servo3, &servo4);
 
     PrintCountDown("Esc arming in", 3);
     EscTimer escTimer(1);
@@ -76,13 +114,15 @@ extern "C" void app_main(void)
     Esc esc4(&escOperator2, GPIO_NUM_26);
     escTimer.EnableAndStartTimer();
     Wait("Esc arming...", 3);
-    EscTest(&esc1, &esc2, &esc3, &esc4);
+    // EscTest(&esc1, &esc2, &esc3, &esc4);
 
     PrintCountDown("Entering loop in", 3);
     int64_t prevTime = esp_timer_get_time();
     int64_t currentTime = prevTime;
     float dt = 0;
     TickType_t xLastWakeTime = xTaskGetTickCount();
+
+    sbus.Flush();
     for (;;)
     {
         // int64_t workTime = esp_timer_get_time();         // [us]
@@ -100,7 +140,7 @@ extern "C" void app_main(void)
             // BlinkLedForever();
         }
 
-         ahrs.Update(dt);
+        ahrs.Update(dt);
         //  ahrs.PrintAccRaw();
         //  ahrs.PrintAccCalibrated();
         //  ahrs.PrintGyroRaw();
@@ -111,16 +151,20 @@ extern "C" void app_main(void)
         //  ahrs.PrintQuaternions();
         // ahrs.PrintEulerAngles();
 
-        // baro.Update(dt);
+        baro.Update(dt);
         // baro.PrintAltVs();
 
         if (sbus.Read())
         {
-            sbus.PrintData();
-            // sbus.PrintTest();
+            // sbus.PrintData();
+            //  sbus.PrintTest();
             float ch1 = sbus.GetAnalog(1, -1.0f, 1.0f);
         }
-        //printf("%d\n", sbus.CheckStatus());
+        // printf("%d\n", sbus.CheckStatus());
+
+        // make this task!
+        // frsky.SetVoltage(battery.GetVoltage() / 1000.0f);
+        // frsky.Operate();
 
         bool throttleCut = false;
         if (throttleCut == true)
