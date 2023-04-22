99148ee1324dd914877eb306897ced6a81cacdf5
onurae <oakin.ae@gmail.com>
2023-04-22 21:36:14 +0300

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -72,18 +72,19 @@ extern "C" void Task1(void *params)
 
     Led led;
     led.Init();
+    led.TurnOn();
 
     size_t size = xPortGetFreeHeapSize();
     printf("FreeHeapSize: ");
     printf("%d\n", size);
 
     I2c i2c(I2C_NUM_0, 19, 23); // Port, SCL, SDA
-    i2c.MasterInit();   // Initialize I2C
+    i2c.MasterInit();           // Initialize I2C
 
     uint16_t freq = 500; // Main loop frequency [Hz]
 
     Ahrs ahrs(&i2c);
-    if (!ahrs.Init(freq*2)) // Sensor sample rate: (freq*2)
+    if (!ahrs.Init(freq * 2)) // Sensor sample rate: (freq*2)
     {
         printf("%s", "Ahrs Error!\n");
         led.BlinkForever();
@@ -99,9 +100,14 @@ extern "C" void Task1(void *params)
         printf("%s", "Baro Error!\n");
         led.BlinkForever();
     }
-    
-    //Sbus sbus;
-    //sbus.Init();
+
+    // Sbus sbus;
+    // sbus.Init();
+    float ch1; // set to failsafe values.
+    float ch2;
+    float ch3;
+    float ch4;
+
 
     // ServoTimer servoTimer(0);
     // ServoOperator servoOperator1(&servoTimer);
@@ -113,52 +119,53 @@ extern "C" void Task1(void *params)
     // servoTimer.EnableAndStartTimer();
     // ServoTest(&servo1, &servo2, &servo3, &servo4);
 
-    //PrintCountDown("Esc arming in", 3);
-    //EscTimer escTimer(1);
-    //EscOperator escOperator1(&escTimer);
-    //Esc esc1(&escOperator1, GPIO_NUM_33);
-    //Esc esc2(&escOperator1, GPIO_NUM_27);
-    //EscOperator escOperator2(&escTimer);
-    //Esc esc3(&escOperator2, GPIO_NUM_25);
-    //Esc esc4(&escOperator2, GPIO_NUM_26);
-    //escTimer.EnableAndStartTimer();
-    //Wait("Esc arming...", 3);
-    // EscTest(&esc1, &esc2, &esc3, &esc4);
+    // PrintCountDown("Esc arming in", 3);
+    // EscTimer escTimer(1);
+    // EscOperator escOperator1(&escTimer);
+    // Esc esc1(&escOperator1, GPIO_NUM_33);
+    // Esc esc2(&escOperator1, GPIO_NUM_27);
+    // EscOperator escOperator2(&escTimer);
+    // Esc esc3(&escOperator2, GPIO_NUM_25);
+    // Esc esc4(&escOperator2, GPIO_NUM_26);
+    // escTimer.EnableAndStartTimer();
+    // Wait("Esc arming...", 3);
+    //  EscTest(&esc1, &esc2, &esc3, &esc4);
 
-    {
-        bool isConverged = false;
-        TickType_t xLastWakeTime = xTaskGetTickCount();
-        while(isConverged == false)
-        {
-            xTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(1000 / freq));
-            ahrs.Update((1000 / freq) * 0.001f);
-            ahrs.PrintEulerAngles();
-            if (false)
-            {
-                isConverged = true;
-            }
-        }
-    }
-    
+    // ! disarm first, arm in the main loop. make class.
+
+    //{
+    //    bool isConverged = false;
+    //    TickType_t xLastWakeTime = xTaskGetTickCount();
+    //    while(isConverged == false)
+    //    {
+    //        xTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(1000 / freq));
+    //        ahrs.Update((1000 / freq) * 0.001f);
+    //        ahrs.PrintEulerAngles();
+    //        if (false)
+    //        {
+    //            isConverged = true;
+    //        }
+    //    }
+    //}
+
+    // xTaskCreate(Task2, "mySecondTask", 4096, NULL, 0, NULL); // Low priority.
+    // task2'yi bekleyebilir, semaphore?
+
     PrintCountDown("Entering loop in", 3);
-    int64_t prevTime = esp_timer_get_time();
-    int64_t currentTime = prevTime;
+    led.Blink(3, 150, 75); // Indicates that entering loop.
+    // sbus.Flush();
+    BaseType_t xWasDelayed;
     float dt = 0;
-
-    //xTaskCreate(Task2, "mySecondTask", 4096, NULL, 0, NULL); // Low priority.
-    //sbus.Flush();
+    int64_t prevTime;
+    int64_t currentTime = esp_timer_get_time();
     TickType_t xLastWakeTime = xTaskGetTickCount();
     while (true)
     {
-        // int64_t workTime = esp_timer_get_time();         // [us]
-        // printf("w: %d\n", (uint16_t)(workTime - currentTime));
-
-        // Wait for the next cycle.
-        BaseType_t xWasDelayed = xTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(1000 / freq));
+        xWasDelayed = xTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(1000 / freq)); // Wait for the next cycle.
         prevTime = currentTime;
         currentTime = esp_timer_get_time();         // [us]
         dt = (currentTime - prevTime) / 1000000.0f; // [s]
-        //int a = ((currentTime - prevTime) != 2000) ? printf("dt: %d\n", (uint16_t)(currentTime - prevTime)) : 0;
+        int a = ((currentTime - prevTime) != 2000) ? printf("dt: %d\n", (uint16_t)(currentTime - prevTime)) : 0;
         if (xWasDelayed == pdFALSE)
         {
             printf("%s", "Deadline missed!\n");
@@ -177,24 +184,28 @@ extern "C" void Task1(void *params)
         // ahrs.PrintEulerAngles();
 
         baro.Update(dt);
-        // baro.PrintAltVs();
+        //baro.PrintAltVs();
 
-        //if (sbus.Read())
+        // if (sbus.Read())
         //{
-        //    // sbus.PrintData();
-        //    //  sbus.PrintTest();
-        //    float ch1 = sbus.GetAnalog(1, -1.0f, 1.0f);
-        //}
-        // printf("%d\n", sbus.CheckStatus());
+        //     // sbus.PrintData();
+        //     //  sbus.PrintTest();
+        //     float ch1 = sbus.GetAnalog(1, -1.0f, 1.0f);
+        // }
+        //  printf("%d\n", sbus.CheckStatus());
 
-        //bool throttleCut = false;
-        //if (throttleCut == true)
+        // bool throttleCut = false;
+        // if (throttleCut == true)
         //{
-        //    esc1.Update(1000);
-        //    esc2.Update(1000);
-        //    esc3.Update(1000);
-        //    esc4.Update(1000);
-        //}
+        //     esc1.Update(1000);
+        //     esc2.Update(1000);
+        //     esc3.Update(1000);
+        //     esc4.Update(1000);
+        // }
+
+        
+        // int64_t workTime = esp_timer_get_time();         // [us]
+        // printf("w: %d\n", (uint16_t)(workTime - currentTime));
     }
 }
 
@@ -202,5 +213,8 @@ extern "C" void app_main(void)
 {
     PrintCountDown("Starting in", 10);
     xTaskCreate(Task1, "myFirstTask", 4096, NULL, 1, NULL); // High priority, periodic task.
-    // while ?
+    while(true) // is this necessary?
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
